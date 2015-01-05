5c8f949310de8613a690a581e955f83a6feea006
Levasseur Thibaud <tlevasseur@asi-testing>
2015-01-05 17:06:23 +0100

    debut de vecteur

--- code/TIMVecteur.m
+++ code/TIMVecteur.m
@@ -1,5 +1,14 @@
 function [ vect ] = TIMVecteur( img )
+n=4;
+[long, larg]=TIMRectangle(img);
+[ ri , rc, centre] = TIMCercle( img );
+[aire, perim] =getConvHullArea( img );
 
+vect=zeros(n,1);
+vect(1)=long/larg;
+vect(2)=aire/(long*larg);
+vect(3)=(long+larg)/perim;
+vect(4)=ri/rc;
 
 end
 
