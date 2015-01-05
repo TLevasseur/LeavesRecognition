4a0f1c3734fc9528e2a62477612607551d8a87d7
Levasseur Thibaud <tlevasseur@asi-testing>
2015-01-05 17:33:24 +0100

    number5

--- code/TIMVecteur.m
+++ code/TIMVecteur.m
@@ -1,14 +1,39 @@
 function [ vect ] = TIMVecteur( img )
-n=4;
+n=5;
 [long, larg]=TIMRectangle(img);
 [ ri , rc, centre] = TIMCercle( img );
 [aire, perim] =getConvHullArea( img );
 
 vect=zeros(n,1);
-vect(1)=long/larg;
-vect(2)=aire/(long*larg);
-vect(3)=(long+larg)/perim;
-vect(4)=ri/rc;
+vect(1)=long/larg;%Aspect ratio
+vect(2)=aire/(long*larg);%Ration d'aire
+vect(3)=(long+larg)/perim;%Ratio de perimetre
+vect(4)=ri/rc;%'Sphéricité'
+
+
+contour = bwmorph(img,'remove');
+Mur=0
+compteur=0;
+for i=1:size(contour,1)
+    for j=1:size(contour,2)
+        if (contour(i,j)==1)
+            Mur=Mur+norm(contour(i,j)-centre);
+            compteur=compteur+1;
+        end
+    end
+end
+Mur=Mur/compteur;
+sigr=0;
+for i=1:size(contour,1)
+    for j=1:size(contour,2)
+        if (contour(i,j)==1)
+            sigr=sigr+norm(contour(i,j)-centre)-Mur;
+        end
+    end
+end
+sigr=sigr/compteur;
+
+vect(5)=Mur/sigr;
 
 end
 
