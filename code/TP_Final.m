2d175c399ad30c2ff90f1eca48d7820642494968
Monet Nicolas <nmonet@asi-testing>
2015-01-06 15:18:19 +0100

    Finalisé Main

--- code/TP_Final.m
+++ code/TP_Final.m
@@ -13,7 +13,23 @@ X = data(:, 2:size(data, 2));
 
 
 %% KPPV
-for i = [1 28]
-    YPeval = kppv(X(i, :), X(2:27, :), Y(2:27), 5, []);
-    disp(YPeval);
+% for i = [1 28]
+%     YPeval = kppv(X(i, :), X(2:27, :), Y(2:27), 5, []);
+%     disp(YPeval);
+% end
+
+
+imgPath = input('Path to TIFF image to compare: ','s');
+if isempty(imgPath)
+    disp('No image specified. Aborting');
+    return;
 end
+
+img = imread(imgPath, 'tiff');
+
+disp('Processing image...');
+vect = TIMVecteur(img)';
+
+disp('Resolving class...');
+YPeval = kppv(vect, X, Y, 5, []);
+disp(sprintf('Une feuille de %s', labelToName(YPeval)));
---
+++ code/labelToName.m
@@ -0,0 +1,11 @@
+function name = labelToName(label)
+
+    switch(label)
+    case 2
+        name = 'Erable';
+    case 6
+        name = 'Ortie';
+    otherwise
+        name = 'Inconnu';
+    end
+end 
